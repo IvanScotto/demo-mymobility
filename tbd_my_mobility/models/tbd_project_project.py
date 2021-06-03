e4645651d7d493897a05d13c329bc260f3a0e36a
Alexandre Rosset <alexandre.rosset@tbdgroup.com>
2021-06-03 12:04:15 +0200

    #23918 [WIP] model project.project

--- tbd_my_mobility/models/tbd_project_project.py
+++ tbd_my_mobility/models/tbd_project_project.py
@@ -12,3 +12,16 @@ class TbdProjectProject(models.Model):
     mymob_agency = fields.Many2many('hr.department', string='Etablissement d\'agence')
     mymob_client = fields.Many2one('res.partner', string='Client', required=True, readonly=True)
     mymob_market = fields.Many2one('contract.contract', string='Marché', required=True, readonly=True)
+
+    @api.onchange('partner_id')
+    def onchange_parner_id(self):
+        if not self.partner_id:
+            self.update({
+                'mymob_client': False,
+                'mymob_market': False
+            })
+            return
+        # values = {
+        #     'mymob_client': addr['invoice']
+        # }
+        # self.update(values)
