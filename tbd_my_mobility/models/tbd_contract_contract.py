c7c38579b0668af108707d8aeb4151b4b38fb441
Alexandre Rosset <alexandre.rosset@tbdgroup.com>
2021-06-04 10:26:57 +0200

    #23918 [WIP] view contract.contract

--- tbd_my_mobility/models/tbd_contract_contract.py
+++ tbd_my_mobility/models/tbd_contract_contract.py
@@ -12,28 +12,19 @@ class TbdContractContract(models.Model):
 
     mymob_partner_invoice_id = fields.Many2one(
         'res.partner', string='Invoice Address')
-    contract_line_ids = fields.One2many(
-        string="Contract lines",
-        comodel_name="contract.line",
-        inverse_name="contract_id",
-        copy=True,
-    )
     mymob_lots = fields.One2many(comodel_name='project.project', inverse_name='mymob_market', string='Lots')
-    mymob_client = fields.One2many(comodel_name='project.project', inverse_name='mymob_client', string='Client')
 
     @api.onchange('partner_id')
     def onchange_partner_id(self):
         if not self.partner_id:
             self.update({
-                'mymob_partner_invoice_id': False,
-                'mymob_client': False
+                'mymob_partner_invoice_id': False
             })
             return
 
         addr = self.partner_id.address_get(['invoice'])
         values = {
-            'mymob_partner_invoice_id': addr['invoice'],
-            'mymob_client': self.partner_id
+            'mymob_partner_invoice_id': addr['invoice']
         }
         self.update(values)
 
--- tbd_my_mobility/models/tbd_project_project.py
+++ tbd_my_mobility/models/tbd_project_project.py
@@ -10,20 +10,29 @@ class TbdProjectProject(models.Model):
     _inherit = "project.project"
 
     mymob_agency = fields.Many2many('hr.department', string='Etablissement d\'agence')
-    # mymob_client = fields.Many2one('res.partner', string='Client', required=True, readonly=True)
-    mymob_client = fields.Many2one('contract.contract', string='Client', required=True, readonly=True)
+    mymob_client = fields.Many2one('res.partner', string='Client', required=True, readonly=True)
     mymob_market = fields.Many2one('contract.contract', string='Marché', required=True, readonly=True)
-    # mymob_client = fields.One2many(comodel_name='contract.contract', inverse_name='partner_id', string='Client')
 
-    @api.onchange('partner_id')
-    def onchange_parner_id(self):
-        if not self.partner_id:
-            self.update({
-                'mymob_client': False,
-                'mymob_market': False
-            })
-            return
+    # @api.onchange('partner_id')
+    # def onchange_parner_id(self):
+    #     if not self.partner_id:
+    #         self.update({
+    #             'mymob_client': False,
+    #             'mymob_market': False
+    #         })
+    #         return
         # values = {
         #     'mymob_client': addr['invoice']
         # }
         # self.update(values)
+
+    @api.onchange('mymob_market')
+    def onchange_mymob_market(self):
+        if not self.mymob_market:
+            self.update({'mymob_client': False})
+
+        values = {
+            'mymob_client': self.mymob_market.partner_id,
+            'partner_id': self.mymob_market.partner_id
+        }
+        self.update(values)
