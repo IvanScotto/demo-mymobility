2d7767aa33bfa2507e8bdcd60a6f0bb1a3e10f80
Alexandre Rosset <alexandre.rosset@tbdgroup.com>
2021-06-04 13:47:48 +0200

    #23918 [WIP] view contract.contract

--- tbd_my_mobility/models/tbd_project_project.py
+++ tbd_my_mobility/models/tbd_project_project.py
@@ -10,8 +10,14 @@ class TbdProjectProject(models.Model):
     _inherit = "project.project"
 
     mymob_agency = fields.Many2many('hr.department', string='Etablissement d\'agence')
-    mymob_client = fields.Many2one('res.partner', string='Client', required=True, readonly=True)
-    mymob_market = fields.Many2one('contract.contract', string='Marché', required=True, readonly=True)
+    mymob_client = fields.Many2one('res.partner', string='Client', readonly=True)
+    mymob_market = fields.Many2one('contract.contract', string='Marché', readonly=True)
+
+    @api.model
+    def create(self, vals):
+        if self.mymob_market.partner_id:
+            vals['mymob_client'] = self.mymob_market.partner_id
+        return super(TbdProjectProject, self).create(vals)
 
     # @api.onchange('partner_id')
     # def onchange_parner_id(self):
@@ -26,12 +32,12 @@ class TbdProjectProject(models.Model):
         # }
         # self.update(values)
 
-    @api.onchange('name')
-    def onchange_name(self):
-        if not self.mymob_market:
-            self.update({'mymob_client': False})
-
-        _logger.critical("TBD ON CHANGE TOTO  NAME %s" % self.mymob_market.partner_id)
+    # @api.onchange('name')
+    # def onchange_name(self):
+    #     if not self.mymob_market:
+    #         self.update({'mymob_client': False})
+    #
+    #     _logger.critical("TBD ON CHANGE TOTO  NAME %s" % self.mymob_market.partner_id)
         # values = {
         #     'mymob_client': self.mymob_market.partner_id.id,
         #     'partner_id': self.mymob_market.partner_id.id
