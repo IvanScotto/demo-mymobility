8dd084d7b8f5adce1805be14bd407aab25d0fcde
Alexandre Rosset <alexandre.rosset@tbdgroup.com>
2021-06-08 14:46:35 +0200

    #23912 [WIP] project.project view

--- tbd_my_mobility/models/tbd_project_project.py
+++ tbd_my_mobility/models/tbd_project_project.py
@@ -14,8 +14,8 @@ class TbdProjectProject(models.Model):
     mymob_agency = fields.Many2one('hr.department', string='Etablissement d\'agence')
     mymob_market = fields.Many2one('contract.contract', string='Marché', readonly=True)
     mymob_map = fields.Many2one('res.partner', string='map address')
-    test_long = fields.Integer(default=150)
-    test_lat = fields.Integer(default=150)
+    test_long = fields.Float(default=47.115983)
+    test_lat = fields.Float(default=2.782795)
 
     @api.onchange('user_id')
     def onchange_partner_id(self):
