28f4cd060c87a39759f6f4ff8864d96ea218f7c4
arosset-tbd <83696632+arosset-tbd@users.noreply.github.com>
2021-06-17 16:20:56 +0200

    24416 fix res_partner, contract_contract

--- tbd_my_mobility/__manifest__.py
+++ tbd_my_mobility/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "My Mobility by TBD",
-    'version': '14.0.0.3',
+    'version': '14.0.0.4',
     'author': "Agence TBD",
     'website': "https://www.agence-tbd.com",
     'category': 'Website',
