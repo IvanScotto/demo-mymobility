b006db305c3873aa942a6b2bd1000ea4acc029f9
arosset-tbd <83696632+arosset-tbd@users.noreply.github.com>
2021-06-18 09:56:54 +0200

    24416 split many2many project.project, xic naming

--- tbd_my_mobility/models/tbd_project_project.py
+++ tbd_my_mobility/models/tbd_project_project.py
@@ -9,14 +9,21 @@ _logger = logging.getLogger(__name__)
 class TbdProjectProject(models.Model):
     _inherit = "project.project"
 
+    # TODO A delete
     mymob_school_student = fields.Many2many('res.partner', string='Etablissement scolaire',
                                     domain=[('mymob_partner_type', 'in', ('school', 'student')), ])
+
+    mymob_school = fields.Many2many('res.partner', 'mymob_school', string='Etablissement scolaire',
+                                    domain=[('mymob_partner_type', '=', 'school'), ])
+    mymob_student = fields.Many2many('res.partner', 'mymob_student', string='Student',
+                                    domain=[('mymob_partner_type', '=', 'student'), ])
     mymob_agency = fields.Many2one('hr.department', string='Etablissement d\'agence')
     mymob_market = fields.Many2one('contract.contract', string='Marché', readonly=True)
     mymob_map = fields.Many2one('res.partner', string='map address')
     label_tasks = fields.Char(string='Use Tasks as', default='Lots',
                               help="Label used for the tasks of the project.", translate=True)
 
+    # TODO A delete
     test_long = fields.Float(default=47.115983)
     test_lat = fields.Float(default=2.782795)
 
